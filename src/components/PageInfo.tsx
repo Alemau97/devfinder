df3953f7460f8200d93215b2379245680d422bfe
Alemau97 <orbitweb.ca@gmail.com>
2024-03-29 16:07:04 -0600

    feat: deleting devfinder stuff

--- src/components/PageInfo.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const PageInfo = () => {
-  return (
-    <div className="bg-blue-900">
-      <h1 className="text-3xl text-white font-bold">devfinder</h1>
-    </div>
-  );
-};
